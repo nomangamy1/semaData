ccaf6278bcea08c6be49f5f76d017db5e14e091c
nomangamy1 <kiplimochege@gmail.com>
2026-02-01 15:41:30 +0300

    Working SignUp auth for both the user and domainowner

--- Backend/models/domainowner.py
+++ Backend/models/domainowner.py
@@ -9,6 +9,7 @@ class DomainOwner(db.Model,UserMixin):
     first_name = db.Column(db.String(64), index=True)
     last_name = db.Column(db.String(64), index=True)
     username = db.Column(db.String(36), unique=True, nullable=False)
+    is_verified = db.Column(db.Boolean, nullable=False, default=False)
     email = db.Column(db.String(64), unique=True, nullable=False)
     domain_field = db.Column(db.String(128), index=True, default='Health')
     reference_number = db.Column(db.String(64), unique=True,nullable=True)
